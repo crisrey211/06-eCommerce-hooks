901903bbc573347c0364f14bbd601aadd61f3b6e
christian_reynaltt <creynaltt@maristak.net>
2023-10-24 22:49:44 +0200

    filtros añadidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
+import React from "react"
 import { Products } from "./components/Products"
-import { products } from "./mocks/products.json"
+import { products as initialProducts } from "./mocks/products.json"
 
 function App() {
+  const [products] = React.useState(initialProducts)
+  const [filters, setFilters] = React.useState({
+    category: "all",
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    return products.filter((product) => {
+      return product.price >= filters.minPrice && (product.category === filters.category || filters.category === "all")
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
 
   return (
     <div>
       <h1>Shopping cart 🛒</h1>
-      <Products products={products} />
+      <Products products={filteredProducts} />
     </div>
   )
 }
